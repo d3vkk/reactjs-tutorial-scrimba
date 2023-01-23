2a9f92530bc93550eb204e9c84a5e6a095118085
d3vkk <donaldkk22@yahoo.com>
2023-01-23 10:25:40 +0300

    chore: Use create-react-app

---
+++ 3-project/src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+
+const root = createRoot(document.querySelector("#root"));
+root.render(<h1>React</h1>);
