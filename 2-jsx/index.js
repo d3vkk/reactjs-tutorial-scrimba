7808f7cac65f100517bd4c1c33d8876694b3202c
d3vkk <donaldkk22@yahoo.com>
2023-01-23 09:24:12 +0300

    refactor: JSX

---
+++ 2-jsx/index.js
@@ -0,0 +1,13 @@
+const navbar = (
+  <nav>
+    <h1>My Website</h1>
+    <ul>
+      <li>Pricing</li>
+      <li>About</li>
+      <li>Contact</li>
+    </ul>
+  </nav>
+);
+console.log(navbar);
+
+ReactDOM.render(navbar, document.getElementById("root"));
